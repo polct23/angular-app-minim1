45153015c0acb8178504e46ca464648db88d2270
AliciaCarmonaLopez <alicia.carmona@estudiantat.upc.edu>
2025-03-17 20:42:19 +0100

    Con buscador que busca por nombre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { PacketComponent } from './packet/packet.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [
   // Define aquí tus rutas
   {path: 'packet-component', component: PacketComponent},
   {path: 'user-component', component: UserComponent},
-  //{ path: '',   redirectTo: '/home-component', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: '',   redirectTo: '/user-component', pathMatch: 'full' },
   {path: '**', component: PageNotFoundComponent},
 ];
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchTerm: string = '';
+  filteredUsers: User[] = [];
 
+  constructor(private route: ActivatedRoute, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.searchTerm = params['query'] || '';
+      this.filterUsers();
+    });
+  }
+
+  filterUsers(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.filteredUsers = users.filter(user =>
+        user.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    });
+
+  }
 }
