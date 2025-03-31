0ffd2f440b328fe85ac6cbc3b56bc4ecbfae14f4
Didac <didac.arevalo@estudiantat.upc.edu>
2025-03-31 14:32:39 +0200

    configuracion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Importa las animaciones
 
 export const appConfig: ApplicationConfig = {
-  providers:
-  [ provideZoneChangeDetection({ eventCoalescing: true }),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    //provideClientHydration(withEventReplay()),
-    provideHttpClient()]
-    };
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000, // Duración de la notificación
+      positionClass: 'toast-bottom-right', // Posición de la notificación
+      preventDuplicates: true, // Evita notificaciones duplicadas
+    }),
+  ],
+};
